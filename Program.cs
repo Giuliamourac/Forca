98ee3db4ec2d4bb3d666e0d51a97939ad0483729
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-03-26 16:30:15 -0300

    Finaliza programa

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿using System
+﻿using System;
 
 namespace Forca
 {
     class Program
     {
+        static void Main(string[] args)
+        { 
 
+        Console.WriteLine("Digite a palavra secreta.");
+        string Senha = Console.ReadLine().ToUpper();
+
+        Console.WriteLine("Digite uma letra.");
+        string Letra = Console.ReadLine().ToUpper();
+
+        bool Existe = Senha.Contains(Letra);
+        Console.WriteLine($"A letra \"{Letra}\" existe na palavra secreta => {Existe}");
+
+        }
     }
 }
